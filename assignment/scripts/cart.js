4bec68b809a2c1a770a5d888da851d99fe437778
Vince Saykao <vincesaykao@Vinces-MacBook-Air.local>
2022-01-09 00:10:43 -0600

    updated basket

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,20 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+
+let basket = ['orange','pumpkin','pineapple'];
+
+function addItem(item, basket) {
+  return true
+}
+
+function listItems() {
+}
+
+function empty() {
+
+}
+
+console.log(`Basket is ${basket}`);
+console.log('Adding apples (expect true)', addItem('apples'));
+console.log(`Basket is now ${basket}`);
